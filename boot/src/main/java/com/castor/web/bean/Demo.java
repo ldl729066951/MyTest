7b04690452a3bf390eb07544b9bce86a839dd7b6
ldl729066951 <729066951@qq.com>
2018-03-05 17:39:00 +0800

    spring jpa mysql

--- boot/src/main/java/com/castor/web/bean/Demo.java
+++ boot/src/main/java/com/castor/web/bean/Demo.java
@@ -1,7 +1,15 @@
 package com.castor.web.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "demo")
+@Entity//加入这个注解，Demo就会进行持久化了，在这里没有对@Table进行配置，请自行配置。
 public class Demo {
 
+	@Id @GeneratedValue
 	private long id;
 	private String name;
 	
