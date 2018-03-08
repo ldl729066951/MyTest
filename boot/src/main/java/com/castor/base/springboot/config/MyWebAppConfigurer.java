9a891dbe956c249c1210b483e95e01efad350534
ldl729066951 <729066951@qq.com>
2018-03-08 08:10:19 +0800

    修改com.castor.springboot.config to com.castor.base.springboot.config

--- boot/src/main/java/com/castor/base/springboot/config/MyWebAppConfigurer.java
+++ boot/src/main/java/com/castor/base/springboot/config/MyWebAppConfigurer.java
@@ -1,4 +1,4 @@
-package com.castor.springboot.config;
+package com.castor.base.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
